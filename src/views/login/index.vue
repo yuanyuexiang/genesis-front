9956f81cf754ce17898958615dafb5f1a2b03c4f
yuanyuexiang <yuanyuexiang@126.com>
2018-02-03 22:56:37 +0800

    update code

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -239,83 +239,84 @@
 </style>
 
 <style rel="stylesheet/scss" lang="scss">
-     .tips{
-      font-size: 14px;
-      color: #fff;
-      margin-bottom: 5px;
-    } 
-    
-    .login-container {
-        height: 100vh;
-        background-color: #2d3a4b;
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 5px;
+  } 
+  .app {
+    overflow: hidden;
+  }
+  .login-container {
+      height: 100vh;
+      background-color: #2d3a4b;
 
-        input:-webkit-autofill {
-            -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
-            -webkit-text-fill-color: #fff !important;
-        }
-        input {
-            background: transparent;
-            border: 1px solid #2d8cf0;
-            -webkit-appearance: none;
-            border-radius: 3px;
-            padding: 12px 5px 12px 15px;
-            color: #eeeeee;
-            height: 47px;
-        }
-        .el-input {
-            display: inline-block;
-            height: 47px;
-            width: 85%;
-        }
-        .svg-container {
-            padding: 6px 5px 6px 15px;
-            color: #889aa4;
-        }
+      input:-webkit-autofill {
+          -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
+          -webkit-text-fill-color: #fff !important;
+      }
+      input {
+          background: transparent;
+          border: 1px solid #2d8cf0;
+          -webkit-appearance: none;
+          border-radius: 3px;
+          padding: 12px 5px 12px 15px;
+          color: #eeeeee;
+          height: 47px;
+      }
+      .el-input {
+          display: inline-block;
+          height: 47px;
+          width: 85%;
+      }
+      .svg-container {
+          padding: 6px 5px 6px 15px;
+          color: #889aa4;
+      }
 
-        .title {
-            font-size: 26px;
-            font-weight: 400;
-            color: #eeeeee;
-            margin: 0px auto 40px auto;
-            text-align: center;
-            font-weight: bold;
-        }
-        .log-logo {
-          left: 0;
-          right: 0;
+      .title {
+          font-size: 26px;
+          font-weight: 400;
+          color: #eeeeee;
+          margin: 0px auto 40px auto;
           text-align: center;
-          color: #2d8cf0;
           font-weight: bold;
-          font-size: 30px;
-          margin: 120px auto;
-          margin-bottom: 30px;
-          float: top;
-        }
-        .login-form {
-          left: 0;
-          right: 0;
-          width: 350px;
-          padding: 35px 35px 15px 35px;
-          margin: 10px auto;
-        }
-        .app-copyright {
-          left: 0;
-          right: 0;
-          bottom:20px;
-          float: bottom;
-          position: absolute;  
-          text-align: center;
-        }
-        .el-form-item {
-            border: 1px solid rgba(255, 255, 255, 0.1);
-            background: rgba(0, 0, 0, 0.1);
-            border-radius: 5px;
-            color: #454545;
-        }
+      }
+      .log-logo {
+        left: 0;
+        right: 0;
+        text-align: center;
+        color: #2d8cf0;
+        font-weight: bold;
+        font-size: 30px;
+        margin: 120px auto;
+        margin-bottom: 30px;
+        float: top;
+      }
+      .login-form {
+        left: 0;
+        right: 0;
+        width: 350px;
+        padding: 35px 35px 15px 35px;
+        margin: 10px auto;
+      }
+      .app-copyright {
+        left: 0;
+        right: 0;
+        bottom:20px;
+        float: bottom;
+        position: absolute;  
+        text-align: center;
+      }
+      .el-form-item {
+          border: 1px solid rgba(255, 255, 255, 0.1);
+          background: rgba(0, 0, 0, 0.1);
+          border-radius: 5px;
+          color: #454545;
+      }
 
-        .forget-pwd {
-            color: #fff;
-        }
-    }
-
+      .forget-pwd {
+          color: #fff;
+      }
+  }
 </style>
