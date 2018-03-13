bc4c508cdaac5850d40938080ad0a016f3c4ed73
yuanyuexiang@126.com <yuanyuexiang@126.com>
2018-03-13 23:28:34 +0800

    update code

---
+++ src/api/material.js
@@ -0,0 +1,50 @@
+import fetch from 'utils/fetch';
+
+export function saveMaterial(data) {
+    return fetch({
+        url: '/genesis/v1/material',
+        method: 'post',
+        data
+    });
+}
+
+export function listMaterial(limit, offset) {
+    const params = {
+        limit,
+        offset
+    };
+    return fetch({
+        url: '/genesis/v1/material',
+        method: 'get',
+        params
+    });
+}
+
+export function getMaterial(MediaID) {
+    return fetch({
+        url: '/genesis/v1/material/' + MediaID,
+        method: 'get'
+    });
+}
+
+export function updateMaterial(MediaID, data) {
+    return fetch({
+        url: '/genesis/v1/material/' + MediaID,
+        method: 'put',
+        data
+    });
+}
+
+export function deleteMaterial(MaterialID) {
+    return fetch({
+        url: '/genesis/v1/material/' + MaterialID,
+        method: 'delete'
+    });
+}
+
+export function getMaterialCount() {
+    return fetch({
+        url: '/genesis/v1/material/count',
+        method: 'get'
+    });
+}
--- src/views/MaterialManage.vue
+++ src/views/MaterialManage.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="animated fadeIn">
+        <!--
         <Tabs>
             <TabPane label="图文库" icon="images">图文库</TabPane>
             <TabPane label="图片库" icon="image">图片库</TabPane>
             <TabPane label="语言库" icon="mic-a">语言库</TabPane>
             <TabPane label="视频库" icon="android-film">视频库</TabPane>
         </Tabs>
+        -->
+        <Row style="background-color: white !important;">
+            <Col span="12">
+            <span>图文素材</span>
+            </Col>
+            <Col span="12" >
+            <span style="text-align: right;display: inline-block;width: 100%;">+图文素材</span>
+            </Col>
+        </Row>
     </div>
 </template>
 
